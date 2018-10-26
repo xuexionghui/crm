8bd3b569d74cc3fea019e86a5c067e4b499bf453
xuexionghui <413669152@qq.com>
2018-10-26 18:00:52 +0800

    123

---
+++ src/com/junlaninfo/service/CustomerServiceImpl.java
@@ -0,0 +1,70 @@
+package com.junlaninfo.service;
+
+import java.util.List;
+
+import javax.persistence.criteria.From;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.junlaninfo.domain.Customer;
+import com.junlaninfo.utils.HibernateUtils;
+
+/**
+*  @author xuexionghui E-mail:413669152@qq.com
+*  @version 创建时间：2018年10月26日 下午3:47:49 
+*  客户的实现类
+*  
+**/
+public class CustomerServiceImpl implements CustomerService {
+    /*
+     * 查询没有和定区关联的客户
+     * @see com.junlaninfo.service.CustomerService#findCustomerNoConnectDecidedzone()
+     */
+	@Override
+	public List<Customer> findCustomerNoConnectDecidedzone() {
+		Session session = HibernateUtils.openSession();
+		String hql="from  customer where decidedZoneId is null";
+		Query query = session.createQuery(hql);
+		List<Customer> customers = query.list();
+		session.close();
+		return customers;
+	}
+	/*
+	 * 查询已和定区关联的客户
+	 * @see com.junlaninfo.service.CustomerService#findCustomerConnectDecidedzone(java.lang.String)
+	 */
+
+	@Override
+	public List<Customer> findCustomerConnectDecidedzone(String decidedZoneId) {
+		Session session = HibernateUtils.openSession();
+		String hql="from customer where decidedZoneId is ? ";   //设置占位符
+		
+		Query query = session.createQuery(hql);
+		query.setString(0, decidedZoneId);   //定区的id值
+		
+		List<Customer> customers = query.list();
+		session.close();
+		return customers;
+	}
+
+	@Override
+	public void makeCustomerConnectDecidedzone(String[] customerIds, String decidedZoneId) {
+		//通过id值，先找到客户，然后将定区的id值设置进去
+		Session session = HibernateUtils.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		if (customerIds!=null) {
+			for (String  customerId : customerIds) {
+				Customer customer=(Customer) session.get(Customer.class, customerId);
+			    customer.setDecidedZoneId(decidedZoneId);
+			}
+		}
+		
+		transaction.commit();
+		session.close();
+		
+	}
+
+}
---
+++ src/com/junlaninfo/utils/HibernateUtils.java
@@ -0,0 +1,29 @@
+package com.junlaninfo.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hibernate 操作工具类
+ * 
+ * @author seawind
+ * 
+ */
+public class HibernateUtils {
+	private static Configuration configuration;
+	private static SessionFactory sessionFactory;
+
+	static {
+		configuration = new Configuration().configure();
+		sessionFactory = configuration.buildSessionFactory();
+	}
+
+	public static Session openSession() {
+		return sessionFactory.openSession();
+	}
+
+	public static void main(String[] args) {
+		openSession();
+	}
+}
